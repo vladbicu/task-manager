7f35a55737cd6ed6b03f109bded59a04f301402a
Vlad Bicu <vladbicu@users.noreply.github.com>
2019-07-19 14:24:39 +0300

    Task schema updates (#1)
    * added new fields at task schema

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const app = require("./app");
 
-const app = express();
 const port = process.env.PORT;
 
 app.listen(port, () => {
--- src/models/task.js
+++ src/models/task.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+const moment = require("moment");
 
 const taskSchema = new mongoose.Schema(
   {
@@ -10,6 +12,19 @@ const taskSchema = new mongoose.Schema(
     completed: {
       type: Boolean,
       default: false
+    },
+    dueDate: {
+      type: Number,
+      validate(value) {
+        if (!moment(value).isValid()) {
+          throw new Error("Date is not valid");
+        }
+      }
+    },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      default: "medium"
     },
     owner: {
       type: mongoose.Schema.Types.ObjectId,
