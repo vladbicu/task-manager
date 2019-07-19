0fcf6388cc51b69a60cf80452e08ddd0a443ae2f
Vlad Bicu <vbicu@signal.co>
2019-07-19 17:20:34 +0300

    fixed prod build

--- server/app.js
+++ server/app.js
@@ -14,11 +14,11 @@ app.use("/api", taskRouter);
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "public")));
+  app.use(express.static(path.join(__dirname, "../public")));
 
   // Handle React routing, return all requests to React app
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "../public", "index.html"));
   });
 }
 
